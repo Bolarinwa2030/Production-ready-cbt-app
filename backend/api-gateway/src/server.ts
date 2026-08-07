85c46aa411541b84ab4220b09ee9298e814c6342
Bolarinwa2030 <hablaw30@gmail.com>
2026-08-07 07:02:38 -0400

    Backend update server.ts

---
+++ backend/api-gateway/src/server.ts
@@ -0,0 +1,22 @@
+import { buildApp } from "./app";
+import { env } from "./config/env";
+
+async function start() {
+  const app = buildApp();
+
+  try {
+    await app.listen({
+      port: env.PORT,
+      host: env.HOST,
+    });
+
+    app.log.info(
+      `API Gateway running at http://${env.HOST}:${env.PORT}`
+    );
+  } catch (error) {
+    app.log.error(error);
+    process.exit(1);
+  }
+}
+
+start();

c0a985148d1ba9e86be0c2362d371869198e466d
Bolarinwa2030 <hablaw30@gmail.com>
2026-08-07 04:46:12 -0400

    Add initial API-Gateway config

--- backend/api-gateway/config/env.ts
+++ backend/api-gateway/config/env.ts
@@ -1,7 +1,20 @@
-API_PORT =
+import dotenv from "dotenv";
+import { z } from "zod";
 
-AUTH_SERVICE_URL =
+// Load variables from the .env file
+dotenv.config();
 
-EXAM_SERVICE_URL =
+// Define the expected environment variables
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3000),
+  HOST: z.string().default("0.0.0.0"),
+  NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
 
-REDIS_URL =
+  AUTH_SERVICE_URL: z.string().url(),
+  EXAM_SERVICE_URL: z.string().url(),
+
+  REDIS_URL: z.string(),
+});
+
+// Validate the environment
+export const env = envSchema.parse(process.env);

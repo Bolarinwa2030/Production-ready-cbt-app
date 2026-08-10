9242698bb67bdf2aab911d5d4b0561e8573d1c5a
Bolarinwa2030 <hablaw30@gmail.com>
2026-08-10 19:02:41 -0400

    API-gateway update: Registered route plugins

--- backend/api-gateway/src/app.ts
+++ backend/api-gateway/src/app.ts
@@ -2,22 +2,22 @@ import Fastify from "fastify";
 import { healthRoute } from "./routes/health.js";
 import requestIdMiddleware from "./middleware/request-id.js";
 import loggerPlugin from "./plugins/logger.js";
+import { authRoute } from "./routes/auth.js";
+import { examRoute } from "./routes/exams.js";
 
 export function buildApp() {
   const app = Fastify({
     logger: true,
   });
 
-  console.log("BUILDING APP");
-
+  
   app.register(requestIdMiddleware);
-  console.log("REGISTERED REQUEST ID");
-
+  
   app.register(loggerPlugin);
-  console.log("REGISTERED LOGGER");
-
+  
   app.register(healthRoute);
-  console.log("REGISTERED HEALTH");
+  app.register(authRoute);
+  app.register(examRoute);
 
   return app;
 }

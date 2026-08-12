a091d42aba71e495c6ae685f96d9a7f7bdce5ae1
Bolarinwa2030 <hablaw30@gmail.com>
2026-08-12 03:52:46 -0400

    Auth-Service: Added .env, config/env.ts

---
+++ backend/auth-service/src/config/env.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const envSchema = z.object({
+    PORT: z.coerce.number().default(3001),
+    HOST: z.string().default("0.0.0.0"),
+    NODE_ENV: z
+        .enum(["development", "test", "production"])
+        .default("development"),
+    
+    DATABASE_URL: z.string().min(1),
+});
+
+export const env = envSchema.parse(process.env);
